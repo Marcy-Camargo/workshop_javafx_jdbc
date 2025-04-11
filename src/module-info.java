a5000bda828f31c93ac1accb017d0dab6a6ff456
Marcy Camargo <marcyelle.cassia@gmail.com>
2025-04-11 15:53:45 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -2,5 +2,6 @@ module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
-	opens application to javafx.graphics, javafx.fxml;	
+	opens application to javafx.graphics, javafx.fxml;
+	opens gui to javafx.fxml;
 }
